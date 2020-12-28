5e975f9d77e0aa1c9d420d5eed12da8c66adf962
aanujj <31202716+aanujj@users.noreply.github.com>
2020-12-29 01:26:50 +0530

    Create slice.go

---
+++ slice.go
@@ -0,0 +1,17 @@
+package main 
+  
+import "fmt"
+  
+func main() { 
+  
+    // Creating a slice 
+    // using the var keyword 
+    var my_slice_1 = []string{"hey anuj here"} 
+  
+    fmt.Println("My Slice 1:", my_slice_1) 
+  
+    // Creating a slice 
+    //using shorthand declaration 
+    my_slice_2 := []int{12, 45, 67, 56, 43, 34, 45} 
+    fmt.Println("My Slice 2:", my_slice_2) 
+} 
