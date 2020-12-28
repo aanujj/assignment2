54dafb7e1540cf9e5878ab11862fb52b5270714c
aanujj <31202716+aanujj@users.noreply.github.com>
2020-12-29 01:23:13 +0530

    Create linked_list.go

---
+++ linked_list.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"container/list"
+)
+
+
+func main() {
+
+	mylist := list.New()
+	mylist.PushBack(1)
+	mylist.PushFront(2)
+	
+	for element := mylist.Front(); element != nil; element = element.Next() {
+		// do something with element.Value
+		if element.Value != 1 {
+			mylist.Remove(element)
+		}
+	}
+	
+	for element := mylist.Front(); element != nil; element = element.Next() {
+		// do something with element.Value
+		fmt.Println(element.Value)
+	}
+
+}
